f25fb53ecd8117a793814b37ba0c9b21e511f7a0
Nicholas Romero <ncrmro@gmail.com>
2025-12-25 05:10:43 -0600

    Update TODO comment for SFX power supply parts

--- src/parts/case_sfx.py
+++ src/parts/case_sfx.py
@@ -1,1 +1,1 @@
-# TODO: Placeholder for SFX power supply parts
+# TODO: Placeholder for SFX PSU based tall tower case with motherboard on one side with IO ports facing down and gpu on otherside also with io ports facing down. Vents on other side
